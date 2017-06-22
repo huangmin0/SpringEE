a58a5210c01f66611fa61c39d035b70219d20e2a
hm <1134048328@qq.com>
2017-06-22 19:43:13 +0800

    233

---
+++ Hibernate-2/src/com/hm/hibernate/entities/HibernateTest.java
@@ -0,0 +1,56 @@
+package com.hm.hibernate.entities;
+
+import static org.junit.Assert.*;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class HibernateTest
+{
+	private SessionFactory sessionFactory;
+	private Session session;
+	private Transaction transaction;
+	
+	@Before
+	public void init(){
+		Configuration configuration=new Configuration().configure();
+		ServiceRegistry serviceRegistry=
+				new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+		sessionFactory=configuration.buildSessionFactory(serviceRegistry);
+		session=sessionFactory.openSession();
+		transaction=session.beginTransaction();
+	}
+	@After
+	public void destroy(){
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+	}
+	
+	
+	@Test
+	public void testSessionFlash()
+	{
+		News news=(News) session.get(News.class, 1);
+		news.setAuthor("Oracle");
+		
+	}
+	@Test
+	public void testSessionCache()
+	{
+		News news=(News) session.get(News.class, 1);
+		System.out.println(news);
+		
+		News news1=(News) session.get(News.class, 1);
+		System.out.println(news1);
+	}
+
+}
